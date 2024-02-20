f644a733008b1575e52db210921370e25b048c6d
Mahmoud <elmahallawy6@gmail.com>
2024-02-19 19:00:27 -0800

    mahmoud

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,7 +25,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				f = 1
+				f = 1;
 				print_array(array, size);
 			}
 		}
