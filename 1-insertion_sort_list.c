c561465418e56ff95d736af7a98e231aecfb57ae
Mahmoud <elmahallawy6@gmail.com>
2024-02-20 18:16:19 -0800

    mahmoud

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,43 @@
 #include "sort.h"
+/**
+ *insertion_sort_list - function to sort data stored in linked list
+ *@list: double pointer to the first node
+ *
+ */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t current = NULL, temp = NULL;
-	
-	if (list == NULL || (*list)->next == NULL || *list == NULL)
+	listint_t *current = NULL, *temp = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
 	current = *list;
 	current = current->next;
-	while(current)
+	while (current)
 	{
-		while (current->prev && current->n > (current->prev)->n)
+		while (current->prev && current->n < (current->prev)->n)
 		{
-			temp = currnt;
-			if(cuuren->next)
+			temp = current;
+			if (current->next)
 			{
-				(current->next)->prev = temp->prev
+				(current->next)->prev = temp->prev;
 			}
 			(current->prev)->next = temp->next;
 			current = current->prev;
 			temp->prev = current->prev;
 			temp->next = current;
-			if(current->prev)
+			if (current->prev)
 			{
-				(cuurent->prev)->next = temp;
+				(current->prev)->next = temp;
 			}
-			cuurent->prev = temp;
-			if(temp->prev == NULL)
+			current->prev = temp;
+			if (temp->prev == NULL)
 			{
 				*list = temp;
 			}
-			print(*list);
+			print_list(*list);
 			current = current->prev;
 
 		}
